e6ef4ff0683dc1043758f36a47110bf74abcfedb
ZakMelouk <zakary.melouk@gmail.com>
2023-11-14 15:05:25 +0100

    Debut main

--- SAE_QUAL_DEV/src/Main.java
+++ SAE_QUAL_DEV/src/Main.java
@@ -4,18 +4,33 @@ public class Main {
 
 
     public static void main(String[] args) {
-        String cde;
+        String cde[];
+        Integer i = 1; //  Numéro de l'instruction
+
         do {
             Scanner scanner = new Scanner(System.in);
-            cde = scanner.nextLine();
+             cde = scanner.nextLine().split(" "); // Divise la chaine tapé dans un tableau
+            if(cde[0].compareTo(i.toString())==0){      // Vérifie si le numéro d'instruction est bien valide
+                i++;                                    //Incrémente pour passer au numéro de l'instruction suivante
+                if (cde[1].compareTo("boardsize ") == 0) {
+                    System.out.println("boardsize");
 
-            if (cde.compareTo("formation") == 0) {
-                System.out.println("La première chaîne est inférieure à la deuxième.");
+                } else if (cde[1].compareTo("clear_board") == 0) {
+                    System.out.println("clear_board");
+                    
+                }else if (cde[1].compareTo("genmove ") == 0) {
+                    System.out.println("clear_board");
 
-            } else if (cde.compareTo("formation") == 0) {
-                System.out.println("La première chaîne est inférieure à la deuxième.");
+                }else if (cde[1].compareTo("play") == 0) {
+                    System.out.println("clear_board");
+                }
             }
-        } while (cde.compareTo("quit") != 0);
+            else {
+                cde = new String [2]; // Pour éviter que la vérification dans le while fasse une erreur;
+                cde[1] = "";          // Pour éviter que la vérification dans le while fasse une erreur;
+                System.out.println("probleme");
+            }
+        } while (cde[1].compareTo("quit") != 0);
     }
 
 }
