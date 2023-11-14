8fec30c9f481335011db0dcef824492074dc76fc
ZakMelouk <zakary.melouk@gmail.com>
2023-11-14 13:14:12 +0100

    Debut main

--- SAE_QUAL_DEV/src/Main.java
+++ SAE_QUAL_DEV/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("test");
+        String cde;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            cde = scanner.nextLine();
+
+            if (cde.compareTo("formation") == 0) {
+                System.out.println("La première chaîne est inférieure à la deuxième.");
+
+            } else if (cde.compareTo("formation") == 0) {
+                System.out.println("La première chaîne est inférieure à la deuxième.");
+            }
+        } while (cde.compareTo("quit") != 0);
     }
+
 }
