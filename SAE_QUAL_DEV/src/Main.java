d6c5b82983319e26e15d3aad15bbfbd1027ebfe1
ZakMelouk <zakary.melouk@gmail.com>
2023-11-14 15:09:13 +0100

    Debut main

--- SAE_QUAL_DEV/src/Main.java
+++ SAE_QUAL_DEV/src/Main.java
@@ -11,24 +11,30 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
              cde = scanner.nextLine().split(" "); // Divise la chaine tapé dans un tableau
             if(cde[0].compareTo(i.toString())==0){      // Vérifie si le numéro d'instruction est bien valide
-                i++;                                    //Incrémente pour passer au numéro de l'instruction suivante
-                if (cde[1].compareTo("boardsize ") == 0) {
+                if (cde[1].compareTo("boardsize") == 0) {
                     System.out.println("boardsize");
+                    i++;//Incrémente pour passer au numéro de l'instruction suivante
 
                 } else if (cde[1].compareTo("clear_board") == 0) {
                     System.out.println("clear_board");
-                    
+                    i++;//Incrémente pour passer au numéro de l'instruction suivante
+
                 }else if (cde[1].compareTo("genmove ") == 0) {
                     System.out.println("clear_board");
+                    i++;//Incrémente pour passer au numéro de l'instruction suivante
 
                 }else if (cde[1].compareTo("play") == 0) {
                     System.out.println("clear_board");
+                    i++;//Incrémente pour passer au numéro de l'instruction suivante
+                }
+                else{
+                    System.out.println("probleme d'instruction");
                 }
             }
             else {
                 cde = new String [2]; // Pour éviter que la vérification dans le while fasse une erreur;
                 cde[1] = "";          // Pour éviter que la vérification dans le while fasse une erreur;
-                System.out.println("probleme");
+                System.out.println("probleme de numéro d'instruction");
             }
         } while (cde[1].compareTo("quit") != 0);
     }
